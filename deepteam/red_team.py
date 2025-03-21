1b47de5765e9493d84d52852623416041c7498d7
penguine <jeffreyip@confident-ai.com>
2025-03-21 13:05:17 -0700

    fix red_team method

--- deepteam/red_team.py
+++ deepteam/red_team.py
@@ -1,7 +1,5 @@
 from typing import List
 
-from deepeval.utils import get_or_create_event_loop
-
 from deepteam.vulnerabilities import BaseVulnerability
 from deepteam.attacks import BaseAttack
 from deepteam.attacks.multi_turn.types import CallbackType
@@ -14,31 +12,18 @@ def red_team(
     attacks: List[BaseAttack],
     attacks_per_vulnerability_type: int = 1,
     ignore_errors: bool = False,
-    run_async: bool = True,
+    run_async: bool = False,
     max_concurrent: int = 10,
 ):
     red_teamer = RedTeamer(
         async_mode=run_async,
         max_concurrent=max_concurrent,
     )
-    if run_async:
-        loop = get_or_create_event_loop()
-        loop.run_until_complete(
-            red_teamer.a_red_team(
-                model_callback=model_callback,
-                vulnerabilities=vulnerabilities,
-                attacks=attacks,
-                attacks_per_vulnerability_type=attacks_per_vulnerability_type,
-                ignore_errors=ignore_errors,
-            )
-        )
-        risk_assessment = red_teamer.risk_assessment
-    else:
-        risk_assessment = red_teamer.red_team(
-            model_callback=model_callback,
-            vulnerabilities=vulnerabilities,
-            attacks=attacks,
-            attacks_per_vulnerability_type=attacks_per_vulnerability_type,
-            ignore_errors=ignore_errors,
-        )
+    risk_assessment = red_teamer.red_team(
+        model_callback=model_callback,
+        vulnerabilities=vulnerabilities,
+        attacks=attacks,
+        attacks_per_vulnerability_type=attacks_per_vulnerability_type,
+        ignore_errors=ignore_errors,
+    )
     return risk_assessment
