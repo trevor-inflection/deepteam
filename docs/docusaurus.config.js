95a7862fceecd3cacf80144f24d15a4fbfda1ec5
penguine <jeffreyip@confident-ai.com>
2025-04-09 20:31:38 +0800

    updated docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,7 +15,7 @@ module.exports = {
   favicon: "img/fav.ico",
 
   // Set the production url of your site here
-  url: "https://docs.confident-ai.com",
+  url: "https://trydeepteam.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
