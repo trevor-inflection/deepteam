029e1f4a7fc4afc4400bc95e1caf05d523e9a923
karan kulshrestha <rajprog007@gmail.com>
2025-08-22 03:24:52 +0530

    fix: also resolve vulnerability mapping

--- deepteam/cli/main.py
+++ deepteam/cli/main.py
@@ -59,7 +59,7 @@ VULN_CLASSES = [
     GraphicContent,
     PersonalSafety,
 ]
-VULN_MAP = {cls().get_name(): cls for cls in VULN_CLASSES}
+VULN_MAP = {cls.__name__: cls for cls in VULN_CLASSES}
 
 ATTACK_CLASSES = [
     Base64,
